bd6f3a701ac8893fde4847ff360d4a6d801903d3
LucasAWorkman <lucas.workman@bsu.edu>
2025-01-10 16:58:32 +0000

    hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 // hello in c for fun, no idea if it works as gcc wont return the print
-int main()
-{	
-	printf("Hello, world");
+int main(){	
+	printf("Hello, world \n");
 	return 0;
 }
 
