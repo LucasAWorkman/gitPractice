2bd6a01f931dab2e5e00fc0ba669b1160df2df1a
LucasAWorkman <lucas.workman@bsu.edu>
2025-01-10 15:41:13 +0000

    hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-userName = input(f"What is your name")
+userName = input("What is your name")
 print(f"Hello, {userName}")
-
+userNumber = input("What is your lucky number")
+print(f"I guess that your lucky number is {userNumber}")
